80888a040d1b97c6f82ebe35d3f183a9bea55d71
RaymondDashWu <raymond31670@hotmail.com>
2019-08-27 18:20:16 -0700

    [ADD] React tutorial step 1+2

---
+++ react-tutorial/src/PageHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PageHeader() {
+    return (
+        <div className="PageHeader">
+            <header>
+                <h1>My Site</h1>
+            </header>
+        </div>
+    )
+}
+
+export default PageHeader;
