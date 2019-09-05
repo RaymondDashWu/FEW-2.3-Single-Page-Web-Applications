87c9fb0039f3e4511f07496bb7ad895c61f5c739
RaymondDashWu <raymond31670@hotmail.com>
2019-09-05 14:21:32 -0700

    [REFACTOR] weather api app

---
+++ react-api-project-master/src/App.js
@@ -0,0 +1,30 @@
+
+import React, { Component } from 'react';
+
+import './App.css';
+import Weather from './Weather';
+
+/** 
+ * This example illustrates a simple react project 
+ * that works with an external API. 
+ * 
+ * Take note of the comments they point common 
+ * problems you will need to solve with React. 
+ * 
+ * There are two ideas here
+ * - Input/Controlled Component Pattern
+ * - Conditionally Rendering components 
+ * 
+ * The project has an input field where a user will
+ * input a zip code. It finds weather data for that
+ * zip and displays it in a component. 
+ * 
+ * */
+
+function App() {
+  return (
+    <Weather />
+  )
+}
+
+export default App;
