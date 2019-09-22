50d6a4680e5974f4c54b2c5aed25c92c234090c8
RaymondDashWu <raymond31670@hotmail.com>
2019-09-21 18:07:37 -0700

    [ADD] User can make new timer

--- react-redux-timers/tmrz-app/src/App.js
+++ react-redux-timers/tmrz-app/src/App.js
@@ -6,6 +6,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 
+import NewTimer from './components/new-timer';
+import ListTimers from './components/list-timers';
+
 const store = createStore(reducers);
 
 class App extends Component {
@@ -16,9 +19,8 @@ class App extends Component {
           <header className="App-header">
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <p className="App-intro">
-            Tmrz
-          </p>
+          <NewTimer />
+          <ListTimers />
         </div>
       </Provider>
     );
