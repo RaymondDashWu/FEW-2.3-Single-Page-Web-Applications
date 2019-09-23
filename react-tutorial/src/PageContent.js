ca6cfc866ae913d602df4135eecb6c46abdc5577
RaymondDashWu <raymond31670@hotmail.com>
2019-09-22 18:07:06 -0700

    [FIX] made code DRYer by mapping objects

--- react-tutorial/src/PageContent.js
+++ react-tutorial/src/PageContent.js
@@ -3,16 +3,21 @@ import Project from './Project';
 
 import './PageContent.css';
 
+const projects = [
+    {title:"Tetris Dots", image: "images/logo192.png", link: "#" }, 
+    {title:"Zombie Server", image: "images/logo192.png", link: "#" },
+    {title:"Amazing Colors", image: "images/logo192.png", link: "#" },
+    {title:"Flip Toggle", image: "images/logo192.png", link: "#" },
+    {title:"121 Second St", image: "images/logo192.png", link: "#" },
+    {title:"Slide Shows", image: "images/logo192.png", link: "#" } 
+]
 
 function PageContent() {
     return (
     <div className="PageContent">
-        <Project title="Tetris Dots" image="images/logo192.png" link="#" />
-        <Project title="Zombie Server" image="images/logo192.png" link="#" />
-        <Project title="Amazing Colors" image="images/logo192.png" link="#" />
-        <Project title="Flip Toggle" image="images/logo192.png" link="#" />
-        <Project title="121 Second St" image="images/logo192.png" link="#" />
-        <Project title="Slide Shows" image="images/logo192.png" link="#" />
+        {(projects && projects.map(({ title, image, link}) => {
+            return <Project title={title} image={image} link={link} />
+        })) || <div>Project not found</div>}
     </div>
     )
 }
