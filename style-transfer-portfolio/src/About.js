d25c32867f40756b59228ef758e4afa976e1967f
RaymondDashWu <raymond31670@hotmail.com>
2020-01-08 15:52:23 -0800

    [ADD] Content for Contact page

--- style-transfer-portfolio/src/About.js
+++ style-transfer-portfolio/src/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 function About() {
   return (
+    // https://github.com/RaymondDashWu/style-transfer-app
+    // https://github.com/VinceMarron/style_transfer
     "TODO About page explaining how this works, link to repo, link to Vince's work"
   );
 }
--- style-transfer-portfolio/src/Contact.js
+++ style-transfer-portfolio/src/Contact.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import './Contact.css'
 
 function Contact() {
   return (
-    "TODO Contact page explaining how to get in contact with me"
+    <div class="contactSection">
+      <a href="mailto:raymond31670@hotmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+      <a href="https://github.com/RaymondDashWu"><FontAwesomeIcon icon={faGithub} /></a>
+      <a href="https://www.linkedin.com/in/raymonddashwu/"><FontAwesomeIcon icon={faLinkedin} /></a>
+    </div>
+    
+    // "TODO Contact page explaining how to get in contact with me"
   );
 }
 
